95bb80000a90ba4a5ff0058b7b96ac246bb1195f
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-05-19 08:33:59 +0200

    Serverless and cache (major)

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const { readFileSync } = require('fs')
-const md = require('markdown-it')()
 const { parse } = require('url')
 const { json, send } = require('micro')
 const listAllShortcuts = require('./lib/list-all-shortcuts')
 const filterShortcuts = require('./lib/filter-shortcuts')
 const renderPage = require('./lib/render-page')
+let cache = {}
 
 module.exports = async (request, response) => {
   const { pathname, query } = await parse(request.url, true)
   const data = request.method === 'POST' ? await json(request) : query
-  const results = Object.values(data).length > 0 ? filterShortcuts(data) : listAllShortcuts()
+  let results = []
+  if (Object.values(data).length > 0) {
+    const cached = cache[data.roles]
+    if (!cached) {
+      const shortcuts = filterShortcuts(data)
+      results = shortcuts
+      cache[data.roles] = shortcuts
+    } else {
+      results = cached
+    }
+  } else {
+    results = listAllShortcuts()
+  }
 
   if (!['/', '/view'].includes(pathname)) {
     response.setHeader('Access-Control-Allow-Origin', '*')
@@ -22,8 +33,5 @@ module.exports = async (request, response) => {
     send(response, 200, results)
   } else if (pathname === '/view') {
     send(response, 200, renderPage(results))
-  } else {
-    const readme = readFileSync(`${__dirname}/README.md`, 'utf-8')
-    send(response, 200, md.render(readme))
   }
 }
